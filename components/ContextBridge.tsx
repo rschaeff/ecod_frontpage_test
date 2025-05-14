9e96b1c1091dbb2679af187ec173aceb1410cddf
R. Dustin Schaeffer <ZoroasterD@gmail.com>
2025-05-14 16:56:36 -0500

    Update ContextBridge.tsx

--- components/ContextBridge.tsx
+++ components/ContextBridge.tsx
@@ -7,14 +7,3 @@ export default function ContextBridge({ children }) {
   return <AppContextProvider>{children}</AppContextProvider>;
 }
 
-// app/page.tsx
-import ContextBridge from '@/components/ContextBridge';
-import ECODHomePage from '@/components/ECODHomePage';
-
-export default function Home() {
-  return (
-    <ContextBridge>
-      <ECODHomePage />
-    </ContextBridge>
-  );
-}
